899a480e7139e64d16c80497838d6c3033f7a823
crew7 <100043632+crew7@users.noreply.github.com>
2023-07-07 09:26:29 -0400

    Swapped to object splatting for emails_query

--- emails_query.ps1
+++ emails_query.ps1
@@ -1,19 +1,19 @@
-function Get-Emails{
+function Get-Emails {
     param (
         [string]$RecipientEmail,
+        [string]$MessageTraceId,
         [string]$SenderEmail
     )
     Clear-Host
+
+    $params = @{PageSize = 5000}
     
-    if ($SenderEmail) {
-            Get-MessageTrace -RecipientAddress $RecipientEmail -SenderAddress $SenderEmail -PageSize 5000 | Select-Object Subject,
+    if ($SenderEmail) { $params += @{SenderAddress = $SenderEmail} }
+    if ($RecipientEmail) { $params += @{RecipientAddress = $RecipientEmail} }
+    if ($MessageTraceId) { $params += @{MessageTraceId = $MessageTraceId} }
+
+    Get-MessageTrace @params | Select-Object Subject,
         @{Name="Received"; Expression={$_.Received.ToLocalTime()}},
         SenderAddress,RecipientAddress,FromIP,Status,
         Size,MessageTraceId | more
-    } else {
-            Get-MessageTrace -RecipientAddress $RecipientEmail -PageSize 5000 | Select-Object Subject,
-        @{Name="Received"; Expression={$_.Received.ToLocalTime()}},
-        SenderAddress,RecipientAddress,FromIP,Status,
-        Size,MessageTraceId | more
-    }
 }
